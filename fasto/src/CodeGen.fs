79c94d24911d33913af2ebf326f375b0765bd185
satannius <tobiasostannius@gmail.com>
2018-02-26 14:21:07 +0100

    Added true/false to CodeGen.fs

--- fasto/src/CodeGen.fs
+++ fasto/src/CodeGen.fs
@@ -172,9 +172,10 @@ let rec compileExp  (e      : TypedExp)
       else
         [ Mips.LUI (place, makeConst (n / 65536))
         ; Mips.ORI (place, place, makeConst (n % 65536)) ]
-  | Constant (BoolVal _, _) ->
+  | Constant (BoolVal b, pos) ->
       (* TODO project task 1: represent `true`/`false` values as `1`/`0` *)
-      failwith "Unimplemented code generation of boolean constants"
+      if b then [Mips.LI (place, "1")] // if b = true
+      else [Mips.LI (place, "0")]
   | Constant (CharVal c, pos) -> [ Mips.LI (place, makeConst (int c)) ]
 
   (* Create/return a label here, collect all string literals of the program
