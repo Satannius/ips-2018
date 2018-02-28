0b32fd5b671d106284bbc42bad9fdf41beb3ead8
Emil <haglerhearns1@gmail.com>
2018-02-28 14:02:41 +0100

    Fixed typo-error in negate

--- fasto/src/CodeGen.fs
+++ fasto/src/CodeGen.fs
@@ -266,7 +266,7 @@ let rec compileExp  (e      : TypedExp)
 
   | Negate (e, pos) ->
       let t = newName "negate"
-      let code = comilpExp e vtable t
+      let code = compileExp e vtable t
       code @ [Mips.XORI(place, t, "1")] @ [Mips.XORI(place, t, "-1")]
 
   | Let (dec, e1, pos) ->
@@ -694,11 +694,11 @@ let rec compileExp  (e      : TypedExp)
       let size_reg = newName "size_reg"
       let len_reg  = newName "len_reg"   (* size of input array *)
       let i_reg    = newName "ind_var"   (* loop counter *)
-      let tmp_reg  = newName "tmp_reg"   
+      let tmp_reg  = newName "tmp_reg"
       let acc_reg  = newName "acc_reg"   (* for several purpose *)
       let loop_beg = newName "loop_beg"
       let loop_end = newName "loop_end"
-      
+
       let inp_doce = compileExp arr_exp vtable inp_it
       let acc_code = compileExp acc_exp vtable acc_reg
       let len_code = [Mips.LW (len_reg, inp_it, "0")] (* the offset is 0 *); Mip.ADDI(inp_it, inp_it, "4")
@@ -709,16 +709,16 @@ let rec compileExp  (e      : TypedExp)
               ; Mips.BGEZ(tmp_reg, loop_end)
               ]
 
-      inp_code @ acc_code @ len_code @ dynalloc (len_reg, palce, elem_type) @ 
-      [Mips.ADDI(res_it, place, "4"); Mips.MOVE(i_reg, "0")] @ 
+      inp_code @ acc_code @ len_code @ dynalloc (len_reg, palce, elem_type) @
+      [Mips.ADDI(res_it, place, "4"); Mips.MOVE(i_reg, "0")] @
       [Mips.LW(tmp_reg, inp_it, "0")] @ applyFunArg (binop, [acc_reg; tmp_reg], vtable, acc_reg, pos) @
-      [Mips.SW(acc_reg, res_it, "0"); Mips.ADDI(res_it, res_it, "4"); 
+      [Mips.SW(acc_reg, res_it, "0"); Mips.ADDI(res_it, res_it, "4");
        Mips.ADDI(inp_it, inp_it, "4"); Mips.ADDI(i_reg, i_reg, "1")] @
       [Mips.J loop_beg; Mips.Lable loop_end]
       *)
       failwith "Unimplemented code generation of scan"
-     
-      
+
+
 
 and applyFunArg ( ff     : TypedFunArg
                 , args   : Mips.reg list
